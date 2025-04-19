76c6a867eb0a11df963afc5cde2a18db751e5db3
nimibi60 <nmb360@foxmail.com>
2025-04-19 22:36:10 +0800

    Add files via upload

--- src/contents/contentScript.js
+++ src/contents/contentScript.js
@@ -1,1 +1,12 @@
-
+import { extractArticle } from './extractArticle';
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "startExtraction") {
+        const articleData = extractArticle();
+        chrome.runtime.sendMessage({
+            action: 'articleExtracted',
+            data: articleData
+        });
+    } else if (message.action === "checkScript") {
+        sendResponse({ status: "scriptAlreadyInjected" });
+    }
+});
